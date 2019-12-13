a44947fe419c586c15a07a8f683df72383ebbade
fkp <2866137+fkp@users.noreply.github.com>
2019-12-13 22:20:20 +0000

    tidys

--- caat-attack.js
+++ caat-attack.js
@@ -7,21 +7,18 @@ function loadedHandler() {
 
 function setup() {
   cta = document.getElementById("cta");
-  animate1();
+  animate();
 }
 
-function animate1() {
-  TweenLite.to(["#mainContainer","#dmains","#slideContent"],0,{autoAlpha:1});
-	//TweenLite.fromTo('#slideContent',1, {x:0}, {x:100});
-	
-  TweenLite.to('#slideContent',5,{x:-2000, y:200, z: 0.1, // use if jitter or shaking is really bad
+function animate() {
+  TweenLite.to(["#mainContainer","#dmains","#slideContent"],0,{autoAlpha:1, x:0, y:0});
+  
+  TweenLite.to('#slideContent',0, {x:0, y:1000});
+  TweenLite.to('#slideContent',5,{x:-2000, y:200, z: 0.1,
        rotationZ: 0.01, // use if jitter or shaking is really bad
        ease:Linear.easeNone});
-  TweenLite.to('#slideContent',5,{x:-1000, y:-200, z: 0.1, // use if jitter or shaking is really bad
+  TweenLite.to('#slideContent',5,{x:-1800, y:100, z: 0.1,
        rotationZ: 0.01, // use if jitter or shaking is really bad
        ease:Linear.easeNone,
 	   delay: 5});
-
- 	//document.getElementById('slideContent').className = "start"
-	//TweenLite.to('#endFrame',1,{autoAlpha:1, delay:0})
 }
