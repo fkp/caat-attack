b3a12df34a682233c49f975f9c6d9639c260df49
fkp <2866137+fkp@users.noreply.github.com>
2019-12-14 20:38:29 +0000

    wip

--- caat-attack.js
+++ caat-attack.js
@@ -13,12 +13,26 @@ function setup() {
 function animate() {
   TweenLite.to(["#mainContainer","#dmains","#slideContent"],0,{autoAlpha:1, x:0, y:0});
   
-  TweenLite.to('#slideContent',0, {x:0, y:1000});
-  TweenLite.to('#slideContent',5,{x:-2000, y:200, z: 0.1,
+  TweenLite.to('#slideContent',0, {x:xPosition(1360), y:yPosition(267)});
+  TweenLite.to('#slideContent',5,{x:xPosition(1529), y:yPosition(251), z: 0.1,
        rotationZ: 0.01, // use if jitter or shaking is really bad
        ease:Linear.easeNone});
-  TweenLite.to('#slideContent',5,{x:-1800, y:100, z: 0.1,
+  TweenLite.to('#slideContent',5,{x:xPosition(1686), y:yPosition(207), z: 0.1,
        rotationZ: 0.01, // use if jitter or shaking is really bad
        ease:Linear.easeNone,
 	   delay: 5});
+  TweenLite.to('#slideContent',5,{x:xPosition(1919), y:yPosition(707), z: 0.1,
+       rotationZ: 0.01, // use if jitter or shaking is really bad
+       ease:Linear.easeNone,
+	   delay: 10});
+}
+
+// x coord: -x means left of viewport is x pixels from the left of the image
+function xPosition(xPx) {
+	return -xPx+250;
+}
+
+// y coord: y means bottom viewport is y pixels from the bottom of the image
+function yPosition(yPx) {
+	return 2000-yPx-250;
 }
