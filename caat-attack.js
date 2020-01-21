f8bd39261e32cfa7c2a579be677226a39247948b
fkp <2866137+fkp@users.noreply.github.com>
2020-01-21 20:48:21 +0000

    More stuff

--- caat-attack.js
+++ caat-attack.js
@@ -26,11 +26,13 @@ function animate() {
   for (i=1; i<coords.length; i++)
     coords[i].duration = calcDuration(coords[i].xcoord,coords[i].ycoord,coords[i-1].xcoord,coords[i-1].ycoord);
 
-  coords.forEach(obj =>
-  {
-    console.log(obj.duration + " " + obj.xcoord + " " + obj.ycoord);
-    tl.to('#slideContent',obj.duration,{x:xPosition(obj.xcoord),y:yPosition(obj.ycoord),z:0.1});
-  });
+  panToCoords(tl, coords);
+
+  // Show hazard container
+  tl.to('#hazardContainer',0.5,{autoAlpha:1});
+  tl.to('#hazardImg',0.5,{autoAlpha:1});
+  // Set hazard text?
+  tl.to('#hazardText',0.5,{text:"Bad crossing!"});
 }
 
 // x coord: -x means left of viewport is x pixels from the left of the image
@@ -41,6 +43,14 @@ function xPosition(xPx) {
 // y coord: y means bottom viewport is y pixels from the bottom of the image
 function yPosition(yPx) {
 	return 2000-yPx-250;
+}
+
+function panToCoords(tl, coords) {
+  coords.forEach(obj =>
+  {
+    //console.log(obj.duration + " " + obj.xcoord + " " + obj.ycoord);
+    tl.to('#slideContent',obj.duration,{x:xPosition(obj.xcoord),y:yPosition(obj.ycoord),z:0.1});
+  });
 }
 
 function calcDuration(x1, y1, x2, y2) {
